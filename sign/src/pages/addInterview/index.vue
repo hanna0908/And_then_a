57cdd8f18c342a10858d7ba38b63a94fd1f5a690
hanna0908 <h5139706n@163.com>
2019-08-06 21:51:13 +0800

    基础版

---
+++ sign/src/pages/addInterview/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h4>面试信息</h4>
+    <ul class="interview">
+      <li>
+        <span>公司名称</span>
+        <input type="text" placeholder="1" />
+      </li>
+      <li>
+        <span>公司电话</span>
+        <input type="text" placeholder="1" />
+      </li>
+      <li>
+        <span>面试时间</span>
+        <input type="text" placeholder="1" />
+      </li>
+      <li>
+        <span>面试地址</span>
+        <input type="text" placeholder="1" />
+      </li>
+    </ul>
+    <h4>备注信息</h4>
+    <div>
+      <textarea />
+    </div>
+    <button class="btn">确认</button>
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style scoped lang="">
+.interview {
+  background: #ffff;
+  border-bottom: 1px solid #ccc;
+  border-top: 1px solid #ccc;
+}
+.interview li {
+  display: flex;
+  height: 100rpx;
+  line-height: 100rpx;
+  border-bottom: 1px solid #ccc;
+  margin-left: 20rpx;
+  color: #9a9a9a;
+}
+.interview li:last-child {
+  border-bottom: none;
+}
+h4 {
+  height: 90rpx;
+  line-height: 90rpx;
+  background: #e0e0e2;
+  padding-left: 20rpx;
+}
+.btn {
+  height: 120rpx;
+  background: #9a9a9a;
+  line-height: 120rpx;
+  color: aliceblue;
+}
+</style>
---
+++ sign/src/pages/home/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrap">
+    <map class="map"></map>
+    <footer class="footer" @click="addexamChange">添加面试</footer>
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    addexamChange() {
+      wx.navigateTo({
+        url: "/pages/addInterview/main"
+      });
+    }
+  },
+  created() {},
+  mounted() {}
+};
+</script>
+<style scoped lang="">
+.wrap {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.tit {
+  height: 90rpx;
+  line-height: 90rpx;
+}
+.map {
+  flex: 1;
+  width: 100%;
+}
+.footer {
+  height: 96rpx;
+  line-height: 96rpx;
+  background: #020816;
+  color: aliceblue;
+  text-align: center;
+}
+</style>
